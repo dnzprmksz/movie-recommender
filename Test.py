f5ef4ed5aeb8d06e89939e064db20f5701309fe8
Deniz Parmaksız <denizparmaksiz@gmail.com>
2016-12-14 18:00:37 +0300

    Resolved the issue with Random Hyperplanes, closes #2.

--- Test.py
+++ Test.py
@@ -1,13 +1,33 @@
+import numpy as np
 from time import time
 from numpy import load
-from scipy.sparse import csr_matrix, csc_matrix
+from scipy.sparse import csr_matrix
+from scipy.spatial.distance import cosine
 
-start_time = time()
+from RandomHyperplanes import calculate_similarity, generate_user_signature
 
-loader = load("UtilityMatrixCSR.npz")
-utility_csr = csr_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
 
-loader = load("UtilityMatrixCSC.npz")
-utility_csc = csc_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
+def test_random_hyperplanes_similarity(i=62500, regenerate=False, vector_count=125):
+	start_time = time()
+	
+	if regenerate:
+		generate_user_signature(vector_count)
+	
+	# Load necessary matrices.
+	loader = load("UtilityMatrixCSR.npz")
+	utility_csr = csr_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
+	signature = np.load("UserSignature.npy")
+	
+	for j in [1, 2, 62500]:
+		u = utility_csr.getrow(i).toarray()
+		v = utility_csr.getrow(j).toarray()
+		angle, distance = calculate_similarity(i, j, signature)
+		
+		print "User %d and Candidate %d" % (i, j)
+		print "Angle and Distance: %d degrees, %f" % (angle, distance)
+		print "Original Distance:  %f\n" % cosine(u, v)
+	
+	print "\n%f seconds elapsed." % (time() - start_time)
 
-print "%f seconds elapsed." % (time() - start_time)
+# Test cases.
+test_random_hyperplanes_similarity()
