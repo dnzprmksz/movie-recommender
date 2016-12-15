c5fa7df8f9d0ae3f62028b11ecad250c786c6682
Deniz Parmaksız <denizparmaksiz@gmail.com>
2016-12-15 14:23:14 +0300

    Fixed a bug that limits only first 40.000 users to be normalized.

--- NormalizeUtilityMatrix.py
+++ NormalizeUtilityMatrix.py
@@ -12,19 +12,19 @@ loader = load("Files/TrainingMatrixCSR.npz")
 utility_csr = csr_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
 
 num_movies = utility_csc.shape[1]
-num_users = utility_csr.shape[1]
+num_users = utility_csr.shape[0]
 
 # Normalize CSC matrix for item-item collaborative filtering usage.
 for index in xrange(1, num_movies):
 	ratings = utility_csc.getcol(index)
-	if ratings.nnz > 0:  # Process the column if it is non-empty.
+	if len(ratings.nonzero()[0]) > 0:  # Process the column if it is non-empty.
 		average = mean(ratings.data)  # Average rating of the movie.
 		utility_csc.data[utility_csc.indptr[index]:utility_csc.indptr[index+1]] -= average  # Normalize each rating of movie.
 
 # Normalize CSR matrix for user-user collaborative filtering usage.
 for index in xrange(1, num_users):
 	ratings = utility_csr.getrow(index)
-	if ratings.nnz > 0:  # Process the row if it is non-empty.
+	if len(ratings.nonzero()[0]) > 0:  # Process the row if it is non-empty.
 		average = mean(ratings.data)  # Average rating of the user.
 		utility_csr.data[utility_csr.indptr[index]:utility_csr.indptr[index+1]] -= average  # Normalize each rating of user.
 
