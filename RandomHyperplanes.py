f6ed591f32b8f0042358722427f0afbf1dccae96
Deniz Parmaksız <denizparmaksiz@gmail.com>
2016-12-15 18:42:01 +0300

    Fixed a bug that prevents random assignment of 0/1s for initial zeros in signature matrix.

--- RandomHyperplanes.py
+++ RandomHyperplanes.py
@@ -1,12 +1,12 @@
 import numpy as np
 from numpy import load
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, csc_matrix
 
 
 def binary_hash(value):
 	key = 0
 	for index in xrange(0, len(value)):
-		if value[index] == 1:
+		if value[index]:
 			key += 2**index
 	return key
 
@@ -23,6 +23,7 @@ def locality_sensitive_hashing(signature, num_bands):
 	
 	# Initialize empty dictionary for hashmap.
 	hashmap = {}
+	keys = []
 	pairs = []
 	
 	loader = load("Files/NormalizedUtilityMatrixCSR.npz")
@@ -41,9 +42,47 @@ def locality_sensitive_hashing(signature, num_bands):
 	# Find and return the candidate pairs.
 	for key, value in hashmap.iteritems():
 		if len(value) > 1:
+			keys.append(key)
 			pairs.append(value)
 	
-	return pairs
+	return keys, pairs
+
+
+def locality_sensitive_hashing_movie(signature, num_bands):
+	num_movies = signature.shape[0]
+	dimension = signature.shape[1]
+	
+	# Calculate the number of columns for each band.
+	if num_movies % num_bands == 0:
+		band_size = num_movies / num_bands
+	else:
+		raise ValueError("Dimension of the signature matrix is not divisible by given number of bands.")
+	
+	# Initialize empty dictionary for hashmap.
+	hashmap = {}
+	keys = []
+	pairs = []
+	
+	loader = load("Files/NormalizedUtilityMatrixCSC.npz")
+	n_utility_csc = csc_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
+	
+	# Hash each band and assign to hash buckets.
+	for movie in xrange(1, num_movies):
+		if len(n_utility_csc.getcol(movie).nonzero()[0]) > 0:
+			for band in xrange(0, num_bands):
+				band_low = band * band_size
+				band_high = band_low + band_size
+				value = signature[movie, band_low:band_high]
+				key = binary_hash(value)
+				hashmap.setdefault(key, []).append(movie)  # Store (key, movie_id) in map.
+	
+	# Find and return the candidate pairs.
+	for key, value in hashmap.iteritems():
+		if len(value) > 1:
+			keys.append(key)
+			pairs.append(value)
+	
+	return keys, pairs
 
 
 # FAILED! Cosine value is not close to the real one, because of the Jaccard similarity at the end.
@@ -78,7 +117,7 @@ def generate_user_signature(num_vectors=120):
 	vectors = 2 * np.random.rand(dimension, num_vectors) - 1
 	
 	# Initialize signature matrix with 0s.
-	signature = np.zeros((num_users, num_vectors), np.int8)
+	signature = np.zeros((num_users, num_vectors))
 	
 	# Apply Random Hyperplanes and Cosine Distance similarity algorithm on utility matrix.
 	for user_id in xrange(1, num_users):
@@ -87,9 +126,53 @@ def generate_user_signature(num_vectors=120):
 		signature[user_id] = descriptor
 	
 	# Post process signature matrix to have only +1/-1 values.
-	signature[signature == 0] = np.random.randint(2)  # Convert 0s into 0 or 1. New 0s will be considered as negative.
+	for i in xrange(0, signature.shape[0]):
+		for j in xrange(0, signature.shape[1]):
+			if signature[i, j] == 0:
+				signature[i, j] = np.random.randint(2)  # Convert 0s into 0 or 1. New 0s will be considered as negative.
 	signature[signature > 0] = 1
 	signature[signature <= 0] = -1
 	
+	# Move signature matrix to a boolean matrix to compress the size.
+	compressed = np.full((num_users, num_vectors), False, dtype=bool)
+	compressed[signature == 1] = True
+	
 	# Save signature matrix.
-	np.save("Files/UserSignature", signature)
+	np.save("Files/UserSignature", compressed)
+
+
+# Min-hashing with random vectors. More vectors produce better approximation.
+def generate_movie_signature(num_vectors=240):
+	# Using normalized utility matrix, since similarity in normalized matrix is more important than vanilla.
+	loader = load("Files/NormalizedUtilityMatrixCSC.npz")
+	utility_csc = csc_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
+	
+	# Get number of users as the dimensions of the vector space.
+	dimension = utility_csc.shape[0]
+	num_movies = utility_csc.shape[1]
+	
+	# Generate random vectors with values between +1 and -1, and form a vertical matrix with them.
+	vectors = 2 * np.random.rand(dimension, num_vectors) - 1
+	
+	# Initialize signature matrix with 0s.
+	signature = np.zeros((num_movies, num_vectors), np.int8)
+	
+	# Apply Random Hyperplanes and Cosine Distance similarity algorithm on utility matrix.
+	for movie_id in xrange(1, num_movies):
+		rating = np.transpose(utility_csc.getcol(movie_id))
+		descriptor = rating.dot(vectors)
+		signature[movie_id] = descriptor
+		
+	# Post process signature matrix to have only +1/-1 values.
+	for i in xrange(0, signature.shape[0]):
+		for j in xrange(0, signature.shape[1]):
+			if signature[i, j] == 0:
+				signature[i, j] = np.random.randint(2)  # Convert 0s into 0 or 1. New 0s will be considered as negative.
+	signature[signature > 0] = 1
+	signature[signature <= 0] = -1
+	
+	# Move signature matrix to a boolean matrix to compress the size.
+	compressed = np.full((num_movies, num_vectors), False, dtype=bool)
+	
+	# Save signature matrix.
+	np.save("Files/MovieSignature", compressed)
--- Test.py
+++ Test.py
@@ -1,30 +1,31 @@
 import numpy as np
 from time import time
 from numpy import load
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, csc_matrix
 from scipy.spatial.distance import cosine
 
 from LatentFactor import estimate_user_rating
-from RandomHyperplanes import generate_user_signature, locality_sensitive_hashing, __calculate_similarity_DO_NOT_USE__
+from RandomHyperplanes import generate_user_signature, locality_sensitive_hashing, __calculate_similarity_DO_NOT_USE__, \
+	locality_sensitive_hashing_movie, generate_movie_signature
 
 
 def test_latent_factor(user_id, movie_id):
 	ranking = estimate_user_rating(user_id, movie_id)
-	print("User %d Movie %d. Estimated ranking: %d" % (user_id, movie_id, ranking))
+	print "User %d Movie %d. Estimated ranking: %d" % (user_id, movie_id, ranking)
 	
 
 def test_lsh(num_bands):
 	start_time = time()
 	signature = np.load("Files/UserSignature.npy")
-	pairs = locality_sensitive_hashing(signature, num_bands)
-	print("---")
+	keys, pairs = locality_sensitive_hashing(signature[0:100, :], num_bands)
+	print "---"
 	
 	loader = load("Files/NormalizedUtilityMatrixCSR.npz")
 	n_utility_csr = csr_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
 	
 	distances = set()
-	for pair in pairs:
-		print(pair)
+	for key, pair in zip(keys, pairs):
+		print key, pair
 		num = len(pair)
 		for i in xrange(0, num):
 			for j in xrange(i+1, num):
@@ -32,14 +33,43 @@ def test_lsh(num_bands):
 				v = n_utility_csr.getrow(pair[j]).toarray()
 				distances.add(cosine(u, v))
 	
-	print("---")
-	print("Distances: %d" % len(distances))
+	print "---"
+	print "Distances: %d" % len(distances)
 	count = 0
 	for i in distances:
 		if i <= 0.5:
 			count += 1
-	print("Distances less than 0.50: %d" % count)
-	print(sorted(distances))
+	print "Distances less than 0.50: %d" % count
+	print sorted(distances)
+
+
+def test_lsh_movie(num_bands):
+	start_time = time()
+	signature = np.load("Files/MovieSignature.npy")
+	keys, pairs = locality_sensitive_hashing_movie(signature[0:2000], num_bands)
+	print "---"
+	
+	loader = load("Files/NormalizedUtilityMatrixCSC.npz")
+	n_utility_csc = csc_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
+	
+	distances = set()
+	for key, pair in zip(keys, pairs):
+		print key, pair
+		num = len(pair)
+		for i in xrange(0, num):
+			for j in xrange(i + 1, num):
+				u = n_utility_csc.getcol(pair[i]).toarray()
+				v = n_utility_csc.getcol(pair[j]).toarray()
+				distances.add(cosine(u, v))
+	
+	print "---"
+	print "Distances: %d" % len(distances)
+	count = 0
+	for i in distances:
+		if i <= 0.5:
+			count += 1
+	print "Distances less than 0.50: %d" % count
+	print sorted(distances)
 
 
 def rmse(acquired_data, test_data):
@@ -48,7 +78,7 @@ def rmse(acquired_data, test_data):
 	return out
 
 
-def test_random_hyperplanes_similarity(i=62500, regenerate=False, vector_count=125):
+def test_random_hyperplanes_similarity(i=62500, regenerate=False, vector_count=120):
 	start_time = time()
 	
 	if regenerate:
@@ -71,8 +101,10 @@ def test_random_hyperplanes_similarity(i=62500, regenerate=False, vector_count=1
 	print "\n%f seconds elapsed." % (time() - start_time)
 
 # Test cases.
-#test_random_hyperplanes_similarity()
-test_lsh(4)
+#test_random_hyperplanes_similarity(regenerate=True)
+#test_lsh(4)
+#generate_movie_signature()
+#test_lsh_movie(8)
 #test_latent_factor(777, 731)
 #test_latent_factor(777, 2027)
 #test_latent_factor(777, 11127)
