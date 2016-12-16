a6c3029b9fd07710aecdf7c4d10b9aa70a364eee
Furkan Usta <furkanusta17@gmail.com>
2016-12-16 17:51:47 +0300

    Added int wrapper for average values to suppress numpy warnings

--- NormalizeUtilityMatrix.py
+++ NormalizeUtilityMatrix.py
@@ -19,14 +19,14 @@ for index in xrange(1, num_movies):
 	ratings = utility_csc.getcol(index)
 	if len(ratings.nonzero()[0]) > 0:  # Process the column if it is non-empty.
 		average = mean(ratings.data)  # Average rating of the movie.
-		utility_csc.data[utility_csc.indptr[index]:utility_csc.indptr[index+1]] -= average  # Normalize each rating of movie.
+		utility_csc.data[utility_csc.indptr[index]:utility_csc.indptr[index+1]] -= int(average)  # Normalize each rating of movie.
 
 # Normalize CSR matrix for user-user collaborative filtering usage.
 for index in xrange(1, num_users):
 	ratings = utility_csr.getrow(index)
 	if len(ratings.nonzero()[0]) > 0:  # Process the row if it is non-empty.
 		average = mean(ratings.data)  # Average rating of the user.
-		utility_csr.data[utility_csr.indptr[index]:utility_csr.indptr[index+1]] -= average  # Normalize each rating of user.
+		utility_csr.data[utility_csr.indptr[index]:utility_csr.indptr[index+1]] -= int(average)  # Normalize each rating of user.
 
 # Save normalized utility matrices.
 np.savez("Files/NormalizedUtilityMatrixCSC", data=utility_csc.data, indices=utility_csc.indices, indptr=utility_csc.indptr, shape=utility_csc.shape)
