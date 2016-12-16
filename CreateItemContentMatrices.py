09c615cfbefd04e0ca99b0a505596649a6c32b44
nazliozge <nazli.ucan@gmail.com>
2016-12-17 00:31:53 +0200

    Bug Fix on Content Base Similarity.

--- CreateItemContentMatrices.py
+++ CreateItemContentMatrices.py
@@ -9,29 +9,37 @@ start_time = time.time()
 conn = mysql.connector.connect(user='root', password='nazli1234', host='127.0.0.1', database='webscale')
 print "Connected to the database successfully."
 cursor = conn.cursor()
-test_cursor = conn.cursor()
+movie_cursor = conn.cursor()
+actor_cursor = conn.cursor()
+director_cursor = conn.cursor()
+genre_cursor = conn.cursor()
+year_cursor = conn.cursor()
 
 # Get movies
-cursor.execute("SELECT * FROM movie_list;")
-movies = cursor.fetchone()
+movie_cursor.execute("SELECT * FROM movie_list;")
+movies = movie_cursor.fetchall()
 num_of_movies = len(movies)
 
-# Get actors
-cursor.execute("SELECT id FROM actor_list;")
-actors = cursor.fetchall()
+# Get number of actors
+actor_cursor.execute("SELECT COUNT(*) FROM actor_list;")
+num_of_actors = actor_cursor.fetchone()[0]
+# print num_of_actors
 
-# Get directors
-cursor.execute("SELECT id FROM director_list;")
-directors = cursor.fetchall()
+# Get number of directors
+director_cursor.execute("SELECT COUNT(*) FROM director_list;")
+num_of_directors = director_cursor.fetchall()[0][0]
+# print num_of_directors
 
-# Get genres
-cursor.execute("SELECT id FROM genre_list;")
-genres = cursor.fetchall()
+# Get number of genres
+genre_cursor.execute("SELECT COUNT(*) FROM genre_list;")
+num_of_genres = genre_cursor.fetchall()[0][0]
+# print num_of_genres
 
 # Calculate the number of release years
 cursor.execute("SELECT release_year FROM movie_list WHERE release_year = (SELECT MAX(release_year) FROM movie_list);")
 query = cursor.fetchone()
 max_year = query[0]
+# print max_year
 
 # Generate the utility matrices in CSR format.
 row_actor = []
@@ -53,46 +61,46 @@ data_year = []
 
 for i in range(0, num_of_movies):
     # Create actor data
-    if len((str(movies[i][7])).split('|')) <= 1:
-        if (str(movies[i][7])).split('|')[7] != '0':
+    if len((str(movies[i][6])).split('|')) <= 1:
+        if (str(movies[i][6])) != '0':
             row_actor.append(i)
-            column_actor.append(movies[i][7])
+            column_actor.append(int(movies[i][6]))
             data_actor.append(1)
     else:
-        for j in len((str(movies[i][7])).split('|')):
+        for j in (str(movies[i][6])).split('|'):
             row_actor.append(i)
-            column_actor.append((str(movies[i][7])).split('|')[j])
+            column_actor.append(j)
             data_actor.append(1)
 
     # Create genre data
-    if len((str(movies[i][6])).split('|')) <= 1:
-        if (str(movies[i][6])).split('|')[6] != '0':
+    if len((str(movies[i][5])).split('|')) <= 1:
+        if (str(movies[i][5])) != '0':
             row_genre.append(i)
-            column_genre.append(movies[i][6])
+            column_genre.append(movies[i][5])
             data_genre.append(1)
     else:
-        for j in len((str(movies[i][6])).split('|')):
+        for j in str(movies[i][5]).split('|'):
             row_genre.append(i)
-            column_genre.append((str(movies[i][6])).split('|')[j])
+            column_genre.append(j)
             data_genre.append(1)
 
-    # Create director data
-    if not movies[i][5] == 0:
-        row_genre.append(i)
-        column_genre.append(movies[i][5])
-        data_genre.append(1)
-        
-    # Create year data
-    if not movies[i][4] == 0:
-        row_year.append(i)
-        column_year.append(movies[i][4])
-        data_year.append(1)
+    # # Create director data
+    # if not movies[i][5] == 0:
+    #     row_genre.append(i)
+    #     column_genre.append(movies[i][5])
+    #     data_genre.append(1)
+    #
+    # # Create year data
+    # if not movies[i][4] == 0:
+    #     row_year.append(i)
+    #     column_year.append(movies[i][4])
+    #     data_year.append(1)
 
 
-actor_based_csr = csr_matrix((data_actor, (row_actor, column_actor)), shape=(num_of_movies + 1, len(actors) + 1));
-genre_based_csr = csr_matrix((data_genre, (row_genre, column_genre)), shape=(num_of_movies + 1, len(genres) + 1));
-director_based_csr = csr_matrix((data_director, (row_director, column_director)),
-                                shape=(num_of_movies + 1, len(directors) + 1));
+actor_based_csr = csr_matrix((data_actor, (row_actor, column_actor)), shape=(num_of_movies + 1, num_of_actors + 1));
+genre_based_csr = csr_matrix((data_genre, (row_genre, column_genre)), shape=(num_of_movies + 1, num_of_genres + 1));
+# director_based_csr = csr_matrix((data_director, (row_director, column_director)), shape=(num_of_movies + 1,
+# len(directors) + 1));
 year_based_csr = csr_matrix((data_year, (row_year, column_year)), shape=(num_of_movies + 1, max_year + 1));
 
 # Save utility matrices.
@@ -100,8 +108,8 @@ np.savez("Files/ActorBasedMatrixCSR", data=actor_based_csr, indices=actor_based_
          shape=actor_based_csr.shape)
 np.savez("Files/GenreBasedMatrixCSR", data=genre_based_csr, indices=genre_based_csr.indices, indptr=genre_based_csr.indptr,
          shape=genre_based_csr.shape)
-np.savez("Files/DirectorBasedMatrixCSR", data=genre_based_csr, indices=genre_based_csr.indices, indptr=genre_based_csr.indptr,
-         shape=genre_based_csr.shape)
+# np.savez("Files/DirectorBasedMatrixCSR", data=genre_based_csr, indices=genre_based_csr.indices,
+# indptr=genre_based_csr.indptr, shape=genre_based_csr.shape)
 np.savez("Files/YearBasedMatrixCSR", data=genre_based_csr, indices=genre_based_csr.indices, indptr=genre_based_csr.indptr,
          shape=genre_based_csr.shape)
 
