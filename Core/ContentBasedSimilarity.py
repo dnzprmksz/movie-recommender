07dd9e9c90403b5fe181e4f2209d851c66d87c53
nazliozge <nazli.ucan@gmail.com>
2016-12-18 00:40:47 +0200

    Improved.
    - Needs bug fix.

--- Core/ContentBasedSimilarity.py
+++ Core/ContentBasedSimilarity.py
@@ -14,28 +14,59 @@ from numpy import load
 # Find most similar 5 movies with the given vector for actors
 # target_movie is type vector
 # id is the given movie_id
+def find_jaccard_similarity(target_movie, current_movie):
+	num_of_actors_in_current_movie = len(current_movie)
+	num_of_actors_in_target_movie = len(target_movie)
+
+	print num_of_actors_in_current_movie
+	print num_of_actors_in_target_movie
+
+	# If curent movie has no actors, return 0
+	if num_of_actors_in_current_movie == 0:
+		return 0
+	common_count = 0
+
+	union = num_of_actors_in_target_movie + num_of_actors_in_current_movie
+	for i in xrange(num_of_actors_in_target_movie):
+		for j in xrange(num_of_actors_in_target_movie):
+			if target_movie[i] == current_movie[j]:
+				common_count += 1
+	print "union: ", union
+	print "comon count :" ,common_count
+	# if no actors are the same, return 0
+	if common_count == 0:
+		return 0
+	#similarity = common_count/(union - common_count)
+	return 0
+
+
 def get_similar_actor(target_movie, id=14):
 	loader = load("Files/ActorBasedMatrixCSR.npz")
 	movie_actor = csr_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
 	num_of_movies = movie_actor.shape[0]
 	similarities = []
+	commons = []
 	# np_tgt = np.array(target_movie)
-	transpose_target = target_movie.T
+	i = 0;
+	target_actors = target_movie.indices
 	for movie in xrange(1, num_of_movies):
-		current_movie = (movie_actor.getrow(movie))
+		current_movie_actors = (movie_actor.getrow(movie)).indices
+		find_jaccard_similarity(target_actors, current_movie_actors)
+		#commons.append(common_count)
+
 		# np_cur = np.array(current_movie)
 		# print np_cur[0:5]
 		# count = count_ones(np_cur, np_tgt)
 		# print type(transpose_target)
 		# print type(current_movie)
-		count = current_movie.dot(transpose_target)
 		# print type(count)
 		# print count.data
-		if len(count.data) > 0:
-			similarities.append([movie, count.data[0]])
-		else:
-			similarities.append([movie, 0])
-	return similarities
+		# if len(count.data) > 0:
+		# 	similarities.append([movie, count.data[0]])
+		# else:
+		# 	similarities.append([movie, 0])
+	#print movie_actor.getrow(1)
+	return commons
 
 
 # Find most similar 5 movies with the given vector for year
@@ -51,7 +82,7 @@ temp[0, 3] = 1
 temp[0, 4] = 1
 temp[0, 5] = 1
 
-print get_similar_actor(temp.getrow(0))
+get_similar_actor(temp.getrow(0))
+#print temp.indices[1]
 
-
-# print count_ones([1,0,0,1,0,0,0,0,1], [1,0,0,1,0,0,1,1,1])
+ #print count_ones([1,0,0,1,0,0,0,0,1], [1,0,0,1,0,0,1,1,1])
