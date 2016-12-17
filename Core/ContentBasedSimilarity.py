d38e9cc22e5ce78eb9490e9c1a9da7450b89b882
Furkan Usta <furkanusta17@gmail.com>
2016-12-18 02:32:06 +0300

    Fixed bug in Actor-based Similarity

--- Core/ContentBasedSimilarity.py
+++ Core/ContentBasedSimilarity.py
@@ -15,57 +15,31 @@ from numpy import load
 # target_movie is type vector
 # id is the given movie_id
 def find_jaccard_similarity(target_movie, current_movie):
-	num_of_actors_in_current_movie = len(current_movie)
-	num_of_actors_in_target_movie = len(target_movie)
-
-	print num_of_actors_in_current_movie
-	print num_of_actors_in_target_movie
-
-	# If curent movie has no actors, return 0
-	if num_of_actors_in_current_movie == 0:
-		return 0
+        # common_count = sum(1 for actor in target_movies if actor in current_movie)
+        # union = len(set(target_movie+current_movie))
 	common_count = 0
-
-	union = num_of_actors_in_target_movie + num_of_actors_in_current_movie
-	for i in xrange(num_of_actors_in_target_movie):
-		for j in xrange(num_of_actors_in_target_movie):
-			if target_movie[i] == current_movie[j]:
-				common_count += 1
-	print "union: ", union
-	print "comon count :" ,common_count
-	# if no actors are the same, return 0
-	if common_count == 0:
-		return 0
-	#similarity = common_count/(union - common_count)
-	return 0
+	union = len(target_movie) + len(current_movie)
+	for actor in target_movie:
+                if actor in current_movie:
+                        common_count += 1
+	similarity = float(common_count)/(union - common_count)
+        return similarity
 
 
 def get_similar_actor(target_movie, id=14):
-	loader = load("Files/ActorBasedMatrixCSR.npz")
+	loader = load("../Files/ActorBasedMatrixCSR.npz")
 	movie_actor = csr_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
 	num_of_movies = movie_actor.shape[0]
 	similarities = []
 	commons = []
 	# np_tgt = np.array(target_movie)
-	i = 0;
 	target_actors = target_movie.indices
 	for movie in xrange(1, num_of_movies):
-		current_movie_actors = (movie_actor.getrow(movie)).indices
-		find_jaccard_similarity(target_actors, current_movie_actors)
-		#commons.append(common_count)
-
-		# np_cur = np.array(current_movie)
-		# print np_cur[0:5]
-		# count = count_ones(np_cur, np_tgt)
-		# print type(transpose_target)
-		# print type(current_movie)
-		# print type(count)
-		# print count.data
-		# if len(count.data) > 0:
-		# 	similarities.append([movie, count.data[0]])
-		# else:
-		# 	similarities.append([movie, 0])
-	#print movie_actor.getrow(1)
+                current_movie_actors = movie_actor[movie].indices
+		similarity = find_jaccard_similarity(target_actors, current_movie_actors)
+                if similarity >= 0.5:
+                        commons.append((movie, similarity))
+        commons.sort(key=(lambda x:x[1]))
 	return commons
 
 
@@ -82,7 +56,7 @@ temp[0, 3] = 1
 temp[0, 4] = 1
 temp[0, 5] = 1
 
-get_similar_actor(temp.getrow(0))
+print get_similar_actor(temp.getrow(0))
 #print temp.indices[1]
 
  #print count_ones([1,0,0,1,0,0,0,0,1], [1,0,0,1,0,0,1,1,1])
