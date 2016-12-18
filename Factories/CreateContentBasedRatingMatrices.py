25b0e33f988ee5889348d2bcb97e7a5b0f5ab314
berilbasakt <berilbasakt@gmail.com>
2016-12-18 20:35:46 +0200

    Content-based rating matrix edited.

--- Factories/CreateContentBasedRatingMatrices.py
+++ Factories/CreateContentBasedRatingMatrices.py
@@ -1,4 +1,4 @@
-import bisect
+import numpy as np
 import time
 
 from scipy.sparse import csc_matrix, csr_matrix
@@ -13,93 +13,56 @@ def create_cb_rating_matrix(content_based_csr_link, normalized_utility_csr_link)
 	utility_csr = csr_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
 	
 	dictionary = {}
-	#utility_csr.shape[0]
-	for user_id in range(5, 8):
+	# Trace all users
+	for user_id in range(1, utility_csr.shape[0]):
+		# Get the rated movies
 		movies_rated_by_user = utility_csr[user_id]
-		print "Rated movies:", movies_rated_by_user
+		# print "Rated movies:"
 		
+		# Trace all the rated movies
 		for movie_id in movies_rated_by_user.indices:
-			actors_of_movie = content_based_csr[movie_id]
+			# Get the contents of the movie (all actors etc.)
+			contents_of_movie = content_based_csr[movie_id]
+			# Get the rating for the movie
 			rating = movies_rated_by_user[0, movie_id]
-			print "Actors:", actors_of_movie.indices, "in movie:", movie_id, "rated as: ", rating
 			
-			for actor_id in actors_of_movie.indices:
-				if dictionary.has_key((user_id, actor_id)):
-					dictionary[user_id, actor_id][0] += rating
-					dictionary[user_id, actor_id][1] += 1
-					print "User:", user_id, "to Actor: ", actor_id, ": ", dictionary[(user_id,actor_id)]
-
+			# Trace all the contents
+			for content_id in contents_of_movie.indices:
+				# If content - user has a value in the dictionary, add the new value on it and increase the count
+				#  count - will be used for avarage calculation
+				if dictionary.has_key((user_id, content_id)):
+					dictionary[user_id, content_id][0] += rating
+					dictionary[user_id, content_id][1] += 1
+					# print "User:", user_id, "to Actor: ", content_id, ": ", dictionary[(user_id, content_id)]
+				# If the content - user key does not exist add a new one with count 1
 				else:
-					dictionary[(user_id, actor_id)] = [rating, 1]
-					print "User:", user_id, "to Actor: ", actor_id, ": ",dictionary[(user_id,actor_id)]
+					dictionary[(user_id, content_id)] = [rating, 1]
+					# print "User:", user_id, "to Actor: ", content_id, ": ", dictionary[(user_id, content_id)]
+		
+		if user_id % 25 == 0:
+			print "25 loops passed"
 	
-	return dictionary
-
-
-# def create_cb_rating_matrix(content_based_csc_link, normalized_utility_csc_link):
-# 	# Load matrices.
-# 	loader = load(content_based_csc_link)
-# 	content_based_csc = csc_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
-# 	loader = load(normalized_utility_csc_link)
-# 	utility_csc = csc_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
-#
-# 	# For the result matrix:
-# 	total_row = []
-# 	total_col = []
-# 	total_data = []
-#
-# 	# Trace all indices of the content type(all actor indices etc.)content_based_csc.shape[1]
-# 	for cb_index in range(1, 2):
-# 		# Get the movies with indexed content(all comedy movies etc.)
-# 		movies_of_content = content_based_csc.getcol(cb_index)
-#
-# 		row = []
-# 		col = []
-# 		data = []
-# 		count = []
-#
-# 		# For each movie that includes that content:
-# 		for movie_index in movies_of_content.indices:
-# 			# Get the ratings of users => (user_index, 0) data
-# 			ratings = utility_csc.getcol(movie_index)
-#
-# 			# Trace ratings, for each user with a rating different than zero:
-# 			for index in range(0, len(ratings.indices)):
-# 				rating = ratings.data[index]
-#
-# 				if not rating == 0:
-# 					user = ratings.indices[index]
-#
-# 					# If the user has already a rating on that content (for another movie)
-# 					if user in row:
-# 						csr_attr_index = row.index(user)
-# 						rating = rating*count[csr_attr_index]  # To get the last sum
-# 						count[csr_attr_index] += 1
-# 						# Get the average of new data and the old one
-# 						rating = float(rating + data[csr_attr_index]) / count[csr_attr_index]
-# 						data[csr_attr_index] = rating
-#
-# 					# Insert the user into list, place the data and content info into related list with same index
-# 					else:
-# 						bisect.insort(row, user)
-# 						csr_attr_index = row.index(user)
-# 						data.insert(csr_attr_index, rating)
-# 						col.insert(csr_attr_index, cb_index)
-# 						count.insert(csr_attr_index, 1)
-#
-# 		total_row += row
-# 		total_col += col
-# 		total_data += data
-#
-# 	content_based_csr = csr_matrix((total_data, (total_row, total_col)), shape=[utility_csc.shape[0], content_based_csc.shape[1]]);
-#
-# 	return content_based_csr
-
+	row = []
+	col = []
+	data = []
+							
+	for key, value in dictionary.iteritems():
+		row.append(key[0])
+		col.append(key[1])
+		rating = dictionary[key][0] / dictionary[key][1]
+		data.append(rating);
+		
+	content_based_csr = csr_matrix((data, (row, col)), shape=[utility_csr.shape[0], content_based_csr.shape[1]]);
+	
+	return content_based_csr
 
 start_time = time.time()
 actor_based_csr = create_cb_rating_matrix("Files/ActorBasedMatrixCSR.npz", "Files/NormalizedUtilityMatrixCSR.npz")
+np.savez("../Files/ActorBasedCSR", data=actor_based_csr.data, indices=actor_based_csr.indices, indptr=actor_based_csr.indptr, shape=actor_based_csr.shape)
 print "%f seconds to finish." % (time.time() - start_time)
 
 #start_time = time.time()
 #genre_based_csr = create_cb_rating_matrix("Files/GenreBasedMatrixCSC.npz", "Files/NormalizedUtilityMatrixCSC.npz")
 #print "%f seconds to finish." % (time.time() - start_time)
+
+
