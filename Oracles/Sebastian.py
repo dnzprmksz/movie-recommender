4d66bb99cf26a82caf4d814ce3a4b001f8741ad8
Deniz Parmaksız <denizparmaksiz@gmail.com>
2016-12-18 03:24:22 +0300

    Enhanced Sebastian's wisdom with the Predictor.

--- Oracles/Sebastian.py
+++ Oracles/Sebastian.py
@@ -1,63 +1,48 @@
-import numpy as np
-from numpy import load
-from scipy.sparse import csr_matrix
-
-from Core import LatentFactor
+from Core.DataLoader import n_utility_csr
+from Core.DataLoader import user_signature
 from Core import RandomHyperplanes
+from Oracles import Predictor
 
 
-def recommend_movie(user_id):
-	# Load necessary matrices.
-	loader = load("../Files/NormalizedUtilityMatrixCSR.npz")
-	n_utility_csr = csr_matrix((loader["data"], loader["indices"], loader["indptr"]), shape=loader["shape"])
-	p = np.load("../Files/SGD_P_120.npy")
-	q = np.load("../Files/SGD_Q_120.npy")
-	user_signature = np.load("../Files/UserSignature.npy")
-
+def recommend_movie(user_id, desired_movie_count=10):
 	candidate_list = []
-
+	
 	# Find the cosine distance of each user to the given user.
 	for candidate_id in xrange(1, len(user_signature)):
-		angle, distance = RandomHyperplanes.calculate_user_similarity(user_id, candidate_id, user_signature)
+		_, distance = RandomHyperplanes.calculate_user_similarity(user_id, candidate_id, user_signature)
 		candidate_list.append((candidate_id, distance))
-
+	
 	# Sort them by distance and select the closest ones.
 	candidate_list.sort(key=(lambda x: x[1]))
-
-	if len(candidate_list) > 4:
-		similar_user_list = candidate_list[0:4]
-	else:
-		similar_user_list = candidate_list
-
+	
+	i = 0
 	movie_list = set()
+	user_movie_id_list = n_utility_csr[user_id].indices
 	# Find the best movies of the most similar users.
-	for i in xrange(0, len(similar_user_list)):
-		similar_user = similar_user_list[i]
+	while i < len(candidate_list) and len(movie_list) < desired_movie_count:
+		similar_user = candidate_list[i]
 		similar_user_id = similar_user[0]
-		similar_user_row = n_utility_csr.getrow(similar_user_id)
-
+		similar_user_row = n_utility_csr[similar_user_id]
+		
 		movie_id_list = similar_user_row.indices
 		data = similar_user_row.data
-
+		num_movies = len(data)
+		
 		# Form the best movie list of the similar users.
-		for j in xrange(0, len(data)):
+		j = 0
+		while j < num_movies and len(movie_list) < desired_movie_count:
 			current_movie_id = movie_id_list[j]
 			rating = data[j]
-			if rating >= 0:  # Check if movie has rated as positive by the user.
+			if rating >= 0 and current_movie_id not in user_movie_id_list:  # Check if movie has rated as positive by the user.
 				movie_list.add(current_movie_id)
-
+			j += 1
+		i += 1
+	
 	recommendation_list = []
 	# Predict the ratings of these movies for the given user.
 	for movie_id in movie_list:
-		prediction = LatentFactor.estimate_user_rating(user_id, movie_id, p, q)
+		prediction = Predictor.predict_rating(user_id, movie_id)
 		recommendation_list.append((movie_id, prediction))  # Append movie to list as (movie_id, rating)
-
+	
 	recommendation_list.sort(key=(lambda x: x[1]), reverse=True)
-	# Remove the movies that the user already rated.
-	user_movie_id_list = n_utility_csr.getrow(user_id).indices
-	for movie_id in user_movie_id_list:
-		for m_tuple in recommendation_list:
-			if m_tuple[0] == movie_id:
-				recommendation_list.remove(m_tuple)
-
 	return recommendation_list
